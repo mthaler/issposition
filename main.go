87bba88289179f00bec2e37f9f0d64f0343af548
Michael Thaler <michael.thaler@dlr.de>
2021-06-18 07:47:16 +0200

    Added download.go.

--- main.go
+++ main.go
@@ -2,39 +2,14 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"net/http"
-	"os"
+	"github.com/mthaler/iss-position/download"
 )
 
 func main() {
 	fileUrl := "https://www.celestrak.com/NORAD/elements/stations.txt"
-	err := DownloadFile("logo.svg", fileUrl)
+	err := download.DownloadFile("logo.svg", fileUrl)
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println("Downloaded: " + fileUrl)
-}
-
-// DownloadFile will download a url to a local file. It's efficient because it will
-// write as it downloads and not load the whole file into memory.
-func DownloadFile(filepath string, url string) error {
-
-	// Get the data
-	resp, err := http.Get(url)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	// Create the file
-	out, err := os.Create(filepath)
-	if err != nil {
-		return err
-	}
-	defer out.Close()
-
-	// Write the body to file
-	_, err = io.Copy(out, resp.Body)
-	return err
 }

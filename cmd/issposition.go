9e36056dce3293f51ef746725c6be48616e269b8
Michael Thaler <michael.thaler@dlr.de>
2021-06-18 11:13:18 +0200

    More refactoring.

--- cmd/issposition.go
+++ cmd/issposition.go
@@ -9,7 +9,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"time"
 )
 
 func main() {
@@ -38,22 +37,7 @@ func main() {
 	}
 
 	iss := satellite.TLEToSat(tle.Line1, tle.Line2, "wgs84")
-	fmt.Println(iss)
-
-	now := time.Now().UTC()
-	fmt.Println(now)
-
-	pos, vel := propagate(iss, now)
-	fmt.Println("Position: ", pos, "velocity:", vel)
-
-	gmst := gsTimeFromDate(now)
-	altitude, velocity, latLng := satellite.ECIToLLA(pos, gmst)
-	fmt.Println("altitude: ", altitude, "velocity: ", velocity, "latLng: ", latLng)
-
-	latLngDeg := satellite.LatLongDeg(latLng)
-	fmt.Println("latLngDeg:", latLngDeg)
-
-	orbit.CreateImage(latLng)
+	orbit.CreateImage(iss)
 
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fs)
@@ -63,12 +47,4 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-}
-
-func propagate(sat satellite.Satellite, t time.Time) (position, velocity satellite.Vector3) {
-	return satellite.Propagate(sat, t.Year(), int(t.Month()), t.Day(), t.Hour(), t.Minute(), t.Second())
-}
-
-func gsTimeFromDate(t time.Time) float64 {
-	return satellite.GSTimeFromDate(t.Year(), int(t.Month()), t.Day(), t.Hour(), t.Minute(), t.Second())
 }
--- internal/orbit/orbit.go
+++ internal/orbit/orbit.go
@@ -8,15 +8,16 @@ import (
 	"log"
 	"math"
 	"os"
+	"time"
 )
 
 const w = 1600
 const h = 800
 
-func CreateImage(pos satellite.LatLong) {
+func CreateImage(iss satellite.Satellite) {
 	dc := gg.NewContext(w, h)
 	drawMap(dc)
-	drawISS(dc, pos)
+	drawISS(dc, iss)
 	err := gg.SaveJPG("images/result.jpg", dc.Image(), 90)
 	if err != nil {
 		log.Fatal(err)
@@ -39,10 +40,22 @@ func drawMap(dc *gg.Context) {
 	dc.DrawImage(img, 0, 0)
 }
 
-func drawISS(dc *gg.Context, pos satellite.LatLong) {
-	x := w/ 2.0 + pos.Longitude *w/ (2.0 * math.Pi)
-	y := h/ 2.0 - pos.Latitude *h/ math.Pi
+func drawISS(dc *gg.Context, iss satellite.Satellite) {
+	now := time.Now().UTC()
+	pos, _ := propagate(iss, now)
+	gmst := gsTimeFromDate(now)
+	_, _, latLng := satellite.ECIToLLA(pos, gmst)
+	x := w/ 2.0 + latLng.Longitude *w/ (2.0 * math.Pi)
+	y := h/ 2.0 - latLng.Latitude *h/ math.Pi
 	dc.DrawCircle(x, y, 15.0)
 	dc.SetRGB(1.0, 1.0, 1.0)
 	dc.Fill()
+}
+
+func propagate(sat satellite.Satellite, t time.Time) (position, velocity satellite.Vector3) {
+	return satellite.Propagate(sat, t.Year(), int(t.Month()), t.Day(), t.Hour(), t.Minute(), t.Second())
+}
+
+func gsTimeFromDate(t time.Time) float64 {
+	return satellite.GSTimeFromDate(t.Year(), int(t.Month()), t.Day(), t.Hour(), t.Minute(), t.Second())
 }
